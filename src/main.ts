714b95a9c465749e41e131892499c82de967b25f
Alexander Pirogov <pirogov27@yahoo.com>
2025-07-19 23:28:03 +0300

    clean

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-// const port = chrome.runtime.connectNative("com.xprgv.chrome-messenger");
-// port.onMessage.addListener((msg) => {
-//     console.log("received:", msg);
-// });
-// port.onDisconnect.addListener(() => {
-//     console.log("disconnected");
-// });
-// port.postMessage({ text: "hello world" });
